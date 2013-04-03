6a89075c0cb48e5d1df7f653a7c62f7b5d72b88f
c <chadreed.intl@gmail.com>
2013-04-03 15:22:45 -0700

    a few things working

--- spec/stackSpec.js
+++ spec/stackSpec.js
@@ -5,11 +5,26 @@ describe("stack", function() {
     stack = makeStack();
   });
 
-  it('should have "add", "remove", and "size" methods', function() {
+  xit('should have "add", "remove", and "size" methods', function() {
     expect(stack.add).toEqual(jasmine.any(Function));
     expect(stack.remove).toEqual(jasmine.any(Function));
     expect(stack.size).toEqual(jasmine.any(Function));
   });
+
+  it('should have initialize with no values in it', function(){
+    expect(stack.size()).toEqual(0);
+  });
+  it('should be able to have values added to it', function(){
+    expect(stack.size()).toEqual(0);
+    stack.add('dog');
+    expect(stack.size()).toEqual(1);
+  });
+  it('should be able to have values unstacked', function(){
+    stack.add('cat');
+    expect(stack.remove()).toEqual('cat');
+  });
+  it('should unstack in the lifo');
+  it('should be able to tell how many items are in the stack');
 
   // add more tests here to test the functionality of stack
 });
